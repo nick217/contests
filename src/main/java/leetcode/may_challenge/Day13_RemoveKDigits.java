a90c95f2ff0fff617579cb14363dc37cbc895501
Nikhil Raheja <nick170294@gmail.com>
2020-05-13 20:39:31 +0530

    Day 13, formatted

--- src/main/java/leetcode/may_challenge/Day13_RemoveKDigits.java
+++ src/main/java/leetcode/may_challenge/Day13_RemoveKDigits.java
@@ -29,60 +29,64 @@ import java.util.Stack;
  */
 public class Day13_RemoveKDigits {
 	public String removeKdigits(String num, int k) {
-        if(k==0) return num;
-        if(k == num.length()) return "0";
-        
-        int top = -1;
-        int length = num.length();
-        
-        char stack[] = new char[length];
-        
-        for(int i = 0; i < length; i++){
-            while(top > -1 && k > 0 && stack[top] > num.charAt(i)){
-                top--;
-                k--;
-            }
-            stack[++top] = num.charAt(i);
-        }
-        while(k > 0){
-            top--;
-            k--;
-        }
-        int end = 0;
-        while(end < top && stack[end] == '0'){
-            end++;
-        }
-        StringBuilder sb = new StringBuilder();
-        while(end <= top){
-            sb.append(stack[end++]);
-        }
-        return sb.toString();
-    }
-	
+		if (k == 0)
+			return num;
+		if (k == num.length())
+			return "0";
+
+		int top = -1;
+		int length = num.length();
+
+		char stack[] = new char[length];
+
+		for (int i = 0; i < length; i++) {
+			while (top > -1 && k > 0 && stack[top] > num.charAt(i)) {
+				top--;
+				k--;
+			}
+			stack[++top] = num.charAt(i);
+		}
+		while (k > 0) {
+			top--;
+			k--;
+		}
+		int end = 0;
+		while (end < top && stack[end] == '0') {
+			end++;
+		}
+		StringBuilder sb = new StringBuilder();
+		while (end <= top) {
+			sb.append(stack[end++]);
+		}
+		return sb.toString();
+	}
+
 	public String removeKdigitsUsingStack(String num, int k) {
-        if(k==0) return num;
-        if(k == num.length()) return "0";
-        
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < num.length(); i++){
-            while(!stack.isEmpty() && k > 0 && stack.peek() > num.charAt(i)){
-                stack.pop();
-                k--;
-            }
-            stack.push(num.charAt(i));
-        }
-        while(k > 0){
-            stack.pop();
-            k--;
-        }
-        StringBuilder str = new StringBuilder();
-        while(!stack.isEmpty()){
-            str.append(stack.pop());
-        }
-        str.reverse();
-        while(str.length() > 1 && str.charAt(0) == '0'){
-            str.deleteCharAt(0);
-        }
-        return str.toString();
-    }
+		if (k == 0)
+			return num;
+		if (k == num.length())
+			return "0";
+
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < num.length(); i++) {
+			while (!stack.isEmpty() && k > 0 && stack.peek() > num.charAt(i)) {
+				stack.pop();
+				k--;
+			}
+			stack.push(num.charAt(i));
+		}
+		while (k > 0) {
+			stack.pop();
+			k--;
+		}
+		StringBuilder str = new StringBuilder();
+		while (!stack.isEmpty()) {
+			str.append(stack.pop());
+		}
+		str.reverse();
+		while (str.length() > 1 && str.charAt(0) == '0') {
+			str.deleteCharAt(0);
+		}
+		return str.toString();
+	}
 }
