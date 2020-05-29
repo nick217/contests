8c2a1ad94337fcd0199075966cb1cf9f0beabf57
Nikhil Raheja <nick170294@gmail.com>
2020-05-29 21:00:21 +0530

    Day 29

---
+++ src/main/java/leetcode/may_challenge/Day29_CourseSchedule.java
@@ -0,0 +1,66 @@
+package leetcode.may_challenge;
+
+import java.util.ArrayList;
+
+/*
+ * 	Link: https://leetcode.com/problems/course-schedule/
+ * 	There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+ * 	Some courses may have prerequisites, for example to take course 0 you have to first take course 1, 
+ * 	which is expressed as a pair: [0,1]
+ * 	Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+	Example 1:
+	Input: numCourses = 2, prerequisites = [[1,0]]
+	Output: true
+	Explanation: There are a total of 2 courses to take. 
+             	 To take course 1 you should have finished course 0. So it is possible.
+
+	Example 2:
+	Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+	Output: false
+	Explanation: There are a total of 2 courses to take. 
+             	 To take course 1 you should have finished course 0, and to take course 0 you should
+             	 also have finished course 1. So it is impossible.
+ 
+	Constraints:
+	The input prerequisites is a graph represented by a list of edges, not adjacency matrices. 
+	Read more about how a graph is represented.
+	You may assume that there are no duplicate edges in the input prerequisites.
+	1 <= numCourses <= 10^5
+ */
+public class Day29_CourseSchedule {
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		ArrayList<Integer> graph[] = new ArrayList[numCourses];
+		for (int i = 0; i < numCourses; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+		for (int[] p : prerequisites) {
+			graph[p[0]].add(p[1]);
+		}
+		int status[] = new int[numCourses];
+		for (int i = 0; i < status.length; i++) {
+			if (status[i] == 0) {
+				if (!dfs(i, graph, status)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean dfs(int node, ArrayList<Integer> graph[], int[] status) {
+		status[node] = 1;
+		for (int n : graph[node]) {
+			if (status[n] == 1) {
+				return false;
+			}
+			if (status[n] == 0) {
+				if (!dfs(n, graph, status)) {
+					return false;
+				}
+			}
+		}
+		status[node] = 2;
+		return true;
+	}
+}
