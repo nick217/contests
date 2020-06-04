53dab84655ac498796b878b62410b6b3ace532f5
Nikhil Raheja <nick170294@gmail.com>
2020-06-04 19:59:13 +0530

    Day 4

---
+++ src/main/java/leetcode/june_challenge/Day4_ReverseString.java
@@ -0,0 +1,29 @@
+package leetcode.june_challenge;
+
+/*
+ * 	Link: https://leetcode.com/problems/reverse-string/
+ * 	Write a function that reverses a string. The input string is given as an array of characters char[].
+ * 	Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * 	You may assume all the characters consist of printable ascii characters.
+
+ 	Example 1:
+	Input: ["h","e","l","l","o"]
+	Output: ["o","l","l","e","h"]
+
+	Example 2:
+	Input: ["H","a","n","n","a","h"]
+	Output: ["h","a","n","n","a","H"]
+ */
+
+public class Day4_ReverseString {
+	public void reverseString(char[] s) {
+		int i = 0;
+		int j = s.length - 1;
+		char temp;
+		while (i < j) {
+			temp = s[i];
+			s[i++] = s[j];
+			s[j--] = temp;
+		}
+	}
+}
